95d8dacb9cf827d78cd724e7677ce9881c13b898
jamessonlps <jamesson1233@gmail.com>
2023-02-11 13:07:05 -0300

    espaco em branco entre numeros

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ def check_sum():
         raise Exception("Insira apenas um parâmetro com a operação entre strings.")
     
     operation: str = sys.argv[1]
+
+    # Verifica se há espaço em branco entre 2 números consecutivos
+    string_with_numbers_spaced = re.findall(pattern=r"[0-9]\s+[0-9]", string=operation)
+    if (len(string_with_numbers_spaced) > 0):
+        raise Exception("Operação inválida.")
     
     # Limpa espaços em branco
     string_clear = operation.replace(" ", "")
     # string_splitted = re.findall(pattern=r"[0-9]|[+-]", string=string_clear)
 
-    result = ast.literal_eval(string_clear)
+    result = eval(string_clear)
     print(result)
     return result
 
