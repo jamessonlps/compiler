f9bf0d3188b1b13c8951115f2c0e9b5f1af40f27
jamessonlps <jamesson1233@gmail.com>
2023-02-15 20:14:44 -0300

    implementa parser

--- main.py
+++ main.py
@@ -1,25 +1,101 @@
 import sys
-import re
-import ast
 
-def check_sum():
-    if (len(sys.argv) > 2):
-        raise Exception("Insira apenas um parâmetro com a operação entre strings.")
+class Token():
+    def __init__(self, token_type: str, value: int) -> None:
+        self.type = token_type
+        self.value = value
+
+
+class Tokenizer():
+    def __init__(self, source: str, position: int = 0) -> None:
+        self.source = source
+        self.position = position
+        self.next: Token = None
     
-    operation: str = sys.argv[1]
+    def selectNext(self) -> None:
+        """
+        Lê o próximo token e atualiza o atributo `next`.
+        """
+        reading = True
+        source_size = len(self.source)
+        token = ""
+        while reading:
+            current_exp_token = self.source[self.position]
+            if (current_exp_token == "+"):
+                self.next = Token(token_type="plus", value=0)
+                self.position += 1
+                reading = False
+            elif (current_exp_token == "-"):
+                self.next = Token(token_type="minus", value=-1)
+                self.position += 1
+                reading = False
+            elif (current_exp_token == " "):
+                self.position += 1
+            elif (current_exp_token.isnumeric()):
+                numeric_token = True
+                while numeric_token:
+                    token += current_exp_token
+                    self.position += 1
+                    if (self.position == source_size):
+                        self.next = Token(token_type="int", value=int(token))
+                        token = ""
+                        numeric_token = False
+                        reading = False
+                    else:
+                        current_exp_token = self.source[self.position]
+                        if not (current_exp_token.isnumeric()):
+                            numeric_token = False
+                            reading = False
+                            self.next = Token(token_type="int", value=int(token))
+                            token = ""
+            else:
+                raise TypeError
 
-    # Verifica se há espaço em branco entre 2 números consecutivos
-    # Ou se começa com algun sinal de operação
-    string_filtered = re.findall(pattern=r"[0-9]\s+[0-9]|^[+-]", string=operation)
-    if (len(string_filtered) > 0):
-        raise Exception("Operação inválida.")
-    
-    # Limpa espaços em branco
-    string_clear = operation.replace(" ", "")
 
-    result = eval(string_clear)
-    print(result)
-    return result
+
+class Parser():
+    tokenizer: Tokenizer = None
+
+    @staticmethod
+    def parseExpression() -> int:
+        """
+        Consome os tokens do `tokenizer` e analisa se
+        a sintaxe está aderente à gramática proposta.
+        Retorna o resultado da expressão analisada.
+        """
+        expression: str = ""
+        while True:
+            try:
+                if (Parser.tokenizer.next.type == "int"):
+                    expression += str(Parser.tokenizer.next.value)
+                elif (Parser.tokenizer.next.type == "plus"):
+                    expression += "+"
+                elif (Parser.tokenizer.next.type == "minus"):
+                    expression += "-"
+                else:
+                    pass
+                Parser.tokenizer.selectNext()
+            except IndexError:
+                break
+        result = eval(expression)
+        print(result)
+        return result
+
+    @staticmethod
+    def run(code) -> int:
+        """
+        Recebe o código fonte como argumento, inicializa 
+        um objeto `Tokenizador`, posiciona no primeiro
+        token e retorna o resultado do `parseExpression()`. 
+        Esse método será chamado pelo `main()`. Ao final verificar
+        se terminou de consumir (token é EOF).
+        """
+        Parser.tokenizer = Tokenizer(source=code)
+        Parser.tokenizer.selectNext()
+        return Parser.parseExpression()
+        
+
+
 
 if __name__ == "__main__":
-    check_sum()
+    Parser.run(sys.argv[1])
